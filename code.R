0077a09621841332c7b5325c4b148c98b4250fef
Mira Sohn <tonton07@gmail.com>
2020-04-14 23:13:34 -0400

    code

--- code.R
+++ code.R
@@ -3,6 +3,9 @@ library(data.table)
 library(stringr)
 library(ggplot2)
 
+h <- head
+g <- glimpse 
+s <- summary
 # master data frame 
 # cell: utilization of mobile phone 
 # inter: utilization of internet 
@@ -34,7 +37,7 @@ cell5 <- gather(cell4, Year,
                 -c('Country Name', 'Country Code', 'Indicator Name', 'Indicator Code'))
 
 cell6 <- cell5 %>% filter(Year %in% as.character(1990:2018))
-
+cell7 <- cell6
 # plotting: cellular subscription in 1990-2018
 plot_by_time_cell <- ggplot(cell6, aes(x = Year, y = Per_100_people, group = cell6[[1]])) + 
         geom_line(aes(color = cell6[[1]])) + 
@@ -51,3 +54,53 @@ gdp1 <- gdp[ ,-c('V64', 'V65')]
 g_name <- as.character(gdp1[1, c(1:4, 35:63)])
 gdp2 <- gdp1[-1, c(1:4, 35:63)]
 names(gdp2) <- g_name
+
+gdp3 <- gdp2 %>% filter(gdp2[[2]] %in% g20)
+keep <- colnames(gdp3[1:4])
+gdp4 <- gather(gdp3, year, gdp_growth, -keep)
+names(gdp4) <- c("Country_Name", "Country_Code", "Indicator_Name", "Indicator_Code", "Year", "GDP_Growth")
+names(cell7) <- c("Country_Name", "Country_Code", "Indicator_Name", "Indicator_Code", "Year", "Per_100_people")
+cell8 <- cell7 %>% mutate(difference = lag(Per_100_people) - Per_100_people)
+
+country_gdp <- function(df, code) {
+        df %>% filter(GDP_Growth < 0, Country_Code == code)
+}
+
+country_cell <- function(df, code) {
+        df %>% filter(Country_Code == code)
+}
+
+# comparing cell change vs gdp growth: inconclusive
+bra_gdp <- country_gdp(gdp4, "BRA")
+arg_gdp <- country_gdp(gdp4, "ARG")
+saudi_gdp <- country_gdp(gdp4, "SAU")
+rus_gdp <- country_gdp(gdp4, "RUS")
+bra_cell <- country_cell(cell8, "BRA")
+arg_cell <- country_cell(cell8, "ARG")
+saudi_cell <- country_cell(cell8, "SAU")
+rus_cell <- country_cell(cell8, "RUS")
+
+# data cleaning for internet
+cell_names <- as.vector(as.character(cell[1, ]))
+names(inter) <- cell_names
+inter1 <- inter[-1, ]
+inter2 <- inter1 %>% filter(inter1[[2]] %in% g20) %>% 
+        select(-as.character(1960:1989), -as.character(2018:2019)) %>% 
+        select("Country Name":"2017")
+names(inter2) <- c("Country_Name", 
+                   "Country_Code", 
+                   "Indicator_Name", 
+                   "Indicator_Code", 
+                   as.character(1990:2017))
+keep1 = c("Country_Name", "Country_Code", "Indicator_Name", "Indicator_Code")
+inter3 <- gather(inter2, Year, Percent, -keep1)
+
+# plotting: internet usage in 1990-2018
+plot_by_time_inter <- ggplot(inter3, aes(x = Year, y = Percent, group = Country_Name)) + 
+        geom_line(aes(color = Country_Name)) + 
+        geom_point(aes(color = Country_Name)) + 
+        ylab('% of population') + 
+        ggtitle('Individuals using the Internet in G20 Countries (1990-2017)') +
+        theme(legend.title = element_blank(), axis.text.x=element_blank())  + 
+        facet_wrap(~ Country_Name) + 
+        theme(legend.position = "none") 
